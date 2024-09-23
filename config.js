c79ab4cbe5a7a7ebdfe70bc5e98b20ec257cbb82
farzamseyedhashem <farzam.seyedhashem@gmail.com>
2024-09-23 14:08:47 +0330

    fewfef

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     // API:'http://45.79.154.223:3000/api',
-    InterAPI:"http://localhost:3000/api",
-    ImageBaseURL:'http://localhost:3000',
-    API:'http://localhost:3000',
+    // InterAPI:"https://localhost:3000/api",
+    ImageBaseURL:'https://api.digizoneshop.com',
+    API:'https://api.digizoneshop.com',
     // ImageBaseURL:'http://localhost:3000',
     // API:'http://localhost:3000',
     // InterAPI:"https://new.bumbleauto.com",
