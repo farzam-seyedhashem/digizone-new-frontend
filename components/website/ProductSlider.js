95b687c145c6d49414761a5ab443bb0ddbac033f
farzamseyedhashem <farzam.seyedhashem@gmail.com>
2024-10-05 14:22:13 +0330

    fewfef

--- components/website/ProductSlider.js
+++ components/website/ProductSlider.js
@@ -5,13 +5,13 @@ import {Swiper, SwiperSlide, useSwiper} from 'swiper/react';
 import IconButton from "@m3/icon_buttons/IconButton";
 import Button from "@m3/buttons/Button";
 import {Dialog} from "@headlessui/react";
-import {Navigation} from "swiper/modules";
+import {Navigation, Zoom} from "swiper/modules";
 import Icon from "@m3/assets/icons/Icon";
 
 export default function ProductSlider({product}) {
     const [images, setImages] = useState([product.thumbnail, ...product.images]);
     const [selectedImage, setSelectedImage] = useState(0);
-    const [isFullScreenDialogOpen,setIsFullScreenDialogOpen] = useState(false);
+    const [isFullScreenDialogOpen, setIsFullScreenDialogOpen] = useState(false);
     const prevRef = useRef(null);
     const nextRef = useRef(null);
     return (
@@ -31,22 +31,32 @@ export default function ProductSlider({product}) {
                 className={"hidden relative md:flex items-center justify-center  overflow-hidden w-full md:w-10/12  md:h-[600px] rounded-[24px] bg-surface-container-light dark:bg-surface-container-dark"}>
                 <Image layout={"fill"} src={ImageBaseURL + images[selectedImage].url}
                        alt={""}/>
-                <Button onClick={()=>setIsFullScreenDialogOpen(true)} icon={"fullscreen"} variant={"tonal"} className={"absolute z-10 right-4 bottom-4"}>
+                <Button onClick={() => setIsFullScreenDialogOpen(true)} icon={"fullscreen"} variant={"tonal"}
+                        className={"absolute z-10 right-4 bottom-4"}>
                     تمام صفحه
                 </Button>
             </div>
-            <Dialog className="z-[1001] fixed inset-0 bg-scrim-light/[25%]" open={isFullScreenDialogOpen} onClose={() =>setIsFullScreenDialogOpen(false)}>
+            <Dialog className="z-[1001] fixed inset-0 bg-scrim-light/[25%]" open={isFullScreenDialogOpen}
+                    onClose={() => setIsFullScreenDialogOpen(false)}>
                 <div
                     className={"flex h-screen  items-center w-full mx-auto  fixed inset-0 bg-surface-container-high-light dark:bg-surface-container-high-dark"}>
-                   <IconButton onClick={()=>setIsFullScreenDialogOpen(false)} groupClassName={"absolute z-10 top-4 left-4"}>
-                       close
-                   </IconButton>
+                    <IconButton onClick={() => setIsFullScreenDialogOpen(false)}
+                                groupClassName={"absolute z-10 top-4 left-4"}>
+                        close
+                    </IconButton>
+                    <div className={"rounded-[16px] py-2 flex items-center pl-6 pr-4 text-label-large font-medium absolute text-on-tertiary-container-dark dark:text-on-tertiary-container-dark bg-tertiary-container-light dark:bg-tertiary-container-dark bottom-4  w-fit left-1/2 transform -translate-x-1/2"}>
+                        <Icon size={20} className={"ml-2 text-[20px]"}>
+                            info
+                        </Icon>
+                        برای زوم شدن عکس دوبار بر روی آن کلیک کنید.
+                    </div>
 
                     <Swiper
                         className={"md:w-8/12 !hidden md:!flex relative  rounded-[24px] !h-fit bg-surface-container-light dark:bg-surface-container-dark"}
                         spaceBetween={0}
                         slidesPerView={1}
-                        modules={[Navigation]}
+                        modules={[Navigation, Zoom]}
+                       zoom={true}
                         navigation={{
                             nextEl: '.swiper-next',
                             prevEl: '.swiper-prev',
@@ -56,19 +66,24 @@ export default function ProductSlider({product}) {
                     >
                         {images.map((image, index) => (
                             <SwiperSlide className={"h-fit"} onClick={() => setSelectedImage(index)} key={index}
-                                         >
-                                <Image width={1920} height={1440} layout={"responsive"} src={ImageBaseURL + image.url}
-                                       alt={""}/>
+                            >
+                                <div className="swiper-zoom-container">
+                                    <Image width={1920} height={1440} layout={"responsive"}
+                                           src={ImageBaseURL + image.url}
+                                           alt={""}/>
+                                </div>
                             </SwiperSlide>
-                        ))}
-                        <button  className={"w-[48px] flex items-center justify-center h-[48px] swiper-next text-primary-light dark:text-primary-dark bg-surface-container-highest-light dark:bg-surface-container-highest-dark rounded-full absolute z-20 left-4 top-1/2 transform -translate-y-1/2 "}>
+                            ))}
+                        <button
+                            className={"w-[48px] flex items-center justify-center h-[48px] swiper-next text-primary-light dark:text-primary-dark bg-surface-container-highest-light dark:bg-surface-container-highest-dark rounded-full absolute z-20 left-4 top-1/2 transform -translate-y-1/2 "}>
                             <Icon>
-                            chevron_left
+                                chevron_left
                             </Icon>
                         </button>
-                        <button className={"w-[48px] h-[48px] flex items-center justify-center swiper-prev text-primary-light dark:text-primary-dark bg-surface-container-highest-light dark:bg-surface-container-highest-dark rounded-full absolute z-20 right-4 top-1/2 transform -translate-y-1/2 "}>
+                        <button
+                            className={"w-[48px] h-[48px] flex items-center justify-center swiper-prev text-primary-light dark:text-primary-dark bg-surface-container-highest-light dark:bg-surface-container-highest-dark rounded-full absolute z-20 right-4 top-1/2 transform -translate-y-1/2 "}>
                             <Icon>
-                            chevron_right
+                                chevron_right
                             </Icon>
                         </button>
                         {/*<div ref={prevRef}>Prev</div>*/}
@@ -79,17 +94,22 @@ export default function ProductSlider({product}) {
                         className={"relative flex md:!hidden w-11/12 rounded-[24px] h-fit bg-surface-container-light dark:bg-surface-container-dark"}
                         spaceBetween={0}
                         slidesPerView={1}
+                        modules={[Navigation, Zoom]}
+                        zoom={true}
+
                         onSlideChange={() => console.log('slide change')}
                         onSwiper={(swiper) => console.log(swiper)}
                     >
                         {images.map((image, index) => (
                             <SwiperSlide onClick={() => setSelectedImage(index)} key={index}
                                          className={`h-fit`}>
-                                <Image width={1920} height={1440} layout={"responsive"} src={ImageBaseURL + image.url}
-                                       alt={""}/>
+                                <div className="swiper-zoom-container">
+                                    <Image width={1920} height={1440} layout={"responsive"}
+                                           src={ImageBaseURL + image.url}
+                                           alt={""}/>
+                                </div>
                             </SwiperSlide>
-                        ))}
-
+                            ))}
 
 
                     </Swiper>
@@ -106,11 +126,13 @@ export default function ProductSlider({product}) {
                 {images.map((image, index) => (
                     <SwiperSlide onClick={() => setSelectedImage(index)} key={index}
                                  className={`h-fit`}>
-                        <Image width={1920} height={1440} layout={"responsive"} src={ImageBaseURL + image.url} alt={""}/>
+                        <Image width={1920} height={1440} layout={"responsive"} src={ImageBaseURL + image.url}
+                               alt={""}/>
                     </SwiperSlide>
                 ))}
 
-                <Button onClick={()=>setIsFullScreenDialogOpen(true)} icon={"fullscreen"} variant={"tonal"} className={"absolute z-10 right-4 bottom-4"}>
+                <Button onClick={() => setIsFullScreenDialogOpen(true)} icon={"fullscreen"} variant={"tonal"}
+                        className={"absolute z-10 right-4 bottom-4"}>
                     تمام صفحه
                 </Button>
             </Swiper>
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import {ThemeProvider} from "@/theme-provider";
 import 'swiper/css'
 import 'swiper/css/navigation';
+import "swiper/css/zoom";
 export default function App({Component, pageProps}) {
     return <ThemeProvider><Component {...pageProps} /></ThemeProvider>;
 }
